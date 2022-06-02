96d5e8ce9da5dc41a2885c0d5a4db65b478b17f9
Numza <csenele@gmail.com>
2022-06-02 10:01:55 -0700

    Added

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-printf(stderr,"with proper grammer, but the outcome is a piece of art,\n");
+fprintf(stdout,"with proper grammer, but the outcome is a piece of art,\n");
 return (0);
 }
