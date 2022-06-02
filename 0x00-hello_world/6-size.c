2a0d9f5adfeb1f549825a1c1aeedb317ef8a8c42
Numza <csenele@gmail.com>
2022-06-02 12:48:43 -0700

    Added 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
 // always return 0
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", (int) sizeof(chat));
+printf("Size of a char: %lu byte(s)\n", (int) sizeof(char));
 printf("Size of a int: %lu byte(s)\n", (int)sizeof(int);
 printf("Size of a long int: %lu byte(s)\n", (int)sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", (int)sizeof(long long int));
