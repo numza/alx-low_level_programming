853b3e6efaa8fa487f209b957994611fafb671f8
Numza <csenele@gmail.com>
2022-06-02 17:15:32 -0700

    Added 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-
+0;276;0c
 /**
  *main -Entry point
  *
@@ -9,6 +9,6 @@
 int main(void)
 {
 char arr[] = "and tha piece of art is useful\" - Dora Korpar,2015-10-19\n";
-write(STDOUT_FILENO, array, 59);
+write(STDOUT_FILENO, arr, 59);
 return (1);
 }
