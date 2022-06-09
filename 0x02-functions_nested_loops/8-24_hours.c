083087e6dcfd3d0355493bf2a6ab9ffed74d1bb3
Senele <43899077+numza@users.noreply.github.com>
2022-06-09 14:01:54 +0200

    crwefrre

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,12 +6,12 @@
 void jack_bauer(void)
 {
 	int hour = 0, minute;
-	
+
 	while (hour <= 23)
 	{
 		minute = 0;
-		
-		while ( minute <= 59)
+
+		while (minute <= 59)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
