5fe20d50e33ae53bb07de7556e89924c8ede56b0
Senele <43899077+numza@users.noreply.github.com>
2022-06-09 13:56:30 +0200

    cscsdfsd

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,26 +8,27 @@
 void times_table(void)
 {
 	int num, mult, prod;
-  num = 0;
+	num = 0;
 	while ( num <= 9)
 	{
 		_putchar('0');
-     mult = 1;
+		mult = 1;
+
 		while (mult <= 9)
 		{
 			_putchar(',');
 			_putchar(' ');
 
 			prod = num * mult;
-
+			
 			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-      _putchar((prod % 10) + '0');
-      mult++;
+			_putchar((prod % 10) + '0');
+			mult++;
 		}
 		_putchar('\n');
-    num++;
+		num++;
 	}
 }
