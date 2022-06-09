267efdd7a4a6987231b0993e8e44d4098e52cad1
Senele <43899077+numza@users.noreply.github.com>
2022-06-09 14:30:40 +0200

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself
    Write a function that prints the n times table, starting with 0.
    
    Prototype: void print_times_table(int n);
    If n is greater than 15 or less than 0 the function should not print anything
    Format: see example

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
+ */
+void print_times_table(int n)
+{
+	int num, mult, prod;
+  
+	if (n >= 0 && n <= 15)
+	{
+    num = 0;
+
+		while (num <= n)
+		{
+			_putchar('0');
+      mult = 1;
+			while (mult <= n)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+        mult++;
+			}
+			_putchar('\n');
+      num++;
+		}
+	}
+}
