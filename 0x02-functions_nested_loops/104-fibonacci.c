c43e7911be8258b71317d561c26f17f860d8e145
Senele <43899077+numza@users.noreply.github.com>
2022-06-09 15:11:43 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
+
 	count = 0;
 
 	while (count < 92)
@@ -28,7 +28,18 @@ int main(void)
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
-	count = 93;
+	
+
+	print_fibunacci(fib1_half1, fib1_half2, fib2_half1, fib2_half2);
+	printf("\n");
+	return (0);
+}
+
+void print_fibonacci(unsigned long fib1_half1,unsigned long fib1_half2,unsigned long fib2_half1,unsigned long fib2_half2)
+{
+	unsigned long half1, half2;
+	
+	int count = 93;
 
 	while (count < 99)
 	{
@@ -50,6 +61,4 @@ int main(void)
 		fib2_half2 = half2;
 		count++;
 	}
-	printf("\n");
-	return (0);
 }
